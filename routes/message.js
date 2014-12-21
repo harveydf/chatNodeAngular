1b45104c2b7a25d35e44fc8f3c6bd784ff19393f
Harvey Forero <harvey.forero@gmail.com>
2014-12-20 22:40:22 -0500

    Add save function to insert into the DB

--- routes/message.js
+++ routes/message.js
@@ -1,28 +1,39 @@
 /**
  * Created by harveydf on 20/12/14.
  */
-var pg = require('pg');
+var pg      = require('pg'),
+    config  = require('../config.json');
 
 (function () {
-    var conString = "postgres://postgres:postgres@localhost/chat";
 
-    exports.findAll = function(callback){
-        var messages = [];
-        pg.connect(conString, function (err, client, done) {
-            var query = client.query('SELECT username, text FROM messages LIMIT 100');
+var conString = config.postgres.host;
 
-            query.on('row', function(row){
-                messages = [{
-                    'userName': row.username,
-                    'messageText': row.text
-                }];
+exports.findAll = function(callback){
+    var messages = [];
+    pg.connect(conString, function (err, client, done) {
+        var query = client.query('SELECT username, text FROM messages LIMIT 100');
+
+        query.on('row', function(row){
+            messages.push({
+                'userName': row.username,
+                'messageText': row.text
             });
-
-            query.on('end', function () {
-                done();
-                return callback(messages);
-            })
         });
-    };
+
+        query.on('end', function () {
+            done();
+            return callback(messages);
+        })
+    });
+};
+
+exports.save = function (data, callback) {
+    pg.connect(conString, function(err, client, done){
+        client.query('INSERT INTO messages(username, text) VALUES($1, $2)', [data.userName, data.messageText], function () {
+            done();
+            return callback();
+        })
+    });
+};
 
 })();
